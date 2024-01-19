0eef91c58e726522e6aa6e4d524a60b173ddd384
ManhV-22 <154359672+ManhV-22@users.noreply.github.com>
2024-01-19 15:15:55 +0700

    Delete teamWork directory

--- teamWork/login.js
+++
@@ -1,9 +0,0 @@
-function checkLogin(){
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    if(username === "admin" && password === "admin"){
-        window.location.href = "home.html"
-    }else{
-        alert("Tai khoan hoac mat khau chua chinh xac!!!")
-    }
-}
